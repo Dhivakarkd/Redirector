af1869d982380c2b4e7b35a8bea0c1534df8b26c
Mani <ganapathymani.2544@gmail.com>
2021-05-03 15:20:09 +0530

    add RestService

--- src/main/java/com/dhivakar/Redirector/Controller/RestService.java
+++ src/main/java/com/dhivakar/Redirector/Controller/RestService.java
@@ -1,7 +1,9 @@
 package com.dhivakar.Redirector.Controller;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
 public class RestService {
@@ -9,5 +11,13 @@ public class RestService {
     @RequestMapping("/")
     public String  HelloWorld(){
         return "Hello World";
+    }
+
+    @RequestMapping("/get/{url}")
+    public RedirectView Redirect(@PathVariable(name="url") String url)
+    {
+        RedirectView redirectView = new RedirectView();
+        redirectView.setUrl("https://www."+url);
+        return redirectView;
     }
 }
