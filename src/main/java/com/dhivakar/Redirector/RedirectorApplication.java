384a536feb35f82f7d1ec5839ec19ddfb4adc036
Dhivakarkd <dhivakar084.github.com>
2021-05-03 12:04:39 +0530

    Initial commit

---
+++ src/main/java/com/dhivakar/Redirector/RedirectorApplication.java
@@ -0,0 +1,13 @@
+package com.dhivakar.Redirector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedirectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedirectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dhivakar/Redirector/RedirectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhivakar.Redirector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedirectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
