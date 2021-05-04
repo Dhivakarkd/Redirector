e6da0645c49c993226e9eff84f95593357b9e267
Mani <ganapathymani.2544@gmail.com>
2021-05-04 16:25:11 +0530

    Update RandomStringGen

--- src/main/java/com/dhivakar/Redirector/Controller/RestService.java
+++ src/main/java/com/dhivakar/Redirector/Controller/RestService.java
@@ -1,6 +1,7 @@
 package com.dhivakar.Redirector.Controller;
 
 import com.dhivakar.Redirector.Model.URLStore;
+import com.dhivakar.Redirector.Service.URLService;
 import com.dhivakar.Redirector.Util.RandomStringGen;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,18 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RestService {
 
-    @RequestMapping("/")
-    public String  HelloWorld(){
-        return "Hello World";
-    }
-
     @RequestMapping("/fetch/{url}")
-    public String StringGen(@PathVariable("url") String url){
-        RandomStringGen randomStringGen= new RandomStringGen();
-            String reURL= randomStringGen.generaor();
-            int id=1;
-        URLStore urlStore = new URLStore(id,url,reURL);
-            id++;
-            return reURL;
+    public void StringGen(@PathVariable("url") String url){
+        URLService urlService = new URLService();
+        urlService.urlhandler(url);
     }
 }
