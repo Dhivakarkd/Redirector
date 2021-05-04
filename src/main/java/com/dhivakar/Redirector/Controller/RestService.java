9df1268d1c3a77ee2a690a1eaff55894c98fb3db
Dhivakarkd <dhivakar084.github.com>
2021-05-04 16:52:05 +0530

    Code CleanUp

--- src/main/java/com/dhivakar/Redirector/Controller/RestService.java
+++ src/main/java/com/dhivakar/Redirector/Controller/RestService.java
@@ -1,8 +1,6 @@
 package com.dhivakar.Redirector.Controller;
 
-import com.dhivakar.Redirector.Model.URLStore;
 import com.dhivakar.Redirector.Service.URLService;
-import com.dhivakar.Redirector.Util.RandomStringGen;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +10,10 @@ public class RestService {
 
 
     @RequestMapping("/fetch/{url}")
-    public void StringGen(@PathVariable("url") String url){
+    public void StringGen(@PathVariable("url") String url) {
         URLService urlService = new URLService();
         urlService.urlhandler(url);
+    }
 
     @RequestMapping("/get")
     public String  HelloWorld(){
