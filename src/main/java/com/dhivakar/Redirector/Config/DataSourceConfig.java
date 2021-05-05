4d550b4fc4cc120530e442f3d80565d2a2bf83cc
Dhivakar R M <46301457+Dhivakarkd@users.noreply.github.com>
2021-05-05 15:56:13 +0530

    Update DataSourceConfig.java

--- src/main/java/com/dhivakar/Redirector/Config/DataSourceConfig.java
+++ src/main/java/com/dhivakar/Redirector/Config/DataSourceConfig.java
@@ -8,12 +8,9 @@ import javax.sql.DataSource;
 
 @Configuration
 public class DataSourceConfig {
-    private final String url ="jdbc:postgresql://ec2-23-22-191-232.compute-1.amazonaws.com:5432/df4meh5t5442kt";
-            //System.getenv("DATASOURCE_URL");
-    private final String username ="yyaqdzrwpsztco";
-                    //System.getenv("DATASOURCE_USERNAME");
-    private final String password ="7085deea7b2fd4d53ef9cb79cb5dcf624bf8b1e5569703a15fd1200f30bdd098";
-                            //System.getenv("DATASOURCE_PASSWORD");
+    private final String url =System.getenv("DATA_URL");
+    private final String username =System.getenv("DATA_USERNAME");
+    private final String password =System.getenv("DATA_PASSWORD");
 
     @Bean
     public DataSource getDatasource() {
