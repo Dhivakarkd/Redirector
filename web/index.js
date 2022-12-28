96d9df05cec6ecae7b703b8613c6cdaec8a5845e
dhivainfo084@gmail.com <dhivainfo084@gmail.com>
2022-12-28 10:18:25 +0530

    docker changes

--- web/index.js
+++ web/index.js
@@ -1,22 +1,58 @@
-const express = require("express"); 
+const express = require("express");
 
-const app = express(); 
-const PORT = process.env.PORT || 4040; 
+const app = express();
+const PORT = process.env.PORT || 4040;
+const redis = require("redis");
 
 let variable = new Map([
+  ["youtube", "https://www.youtube.com/?gl=IN"],
+  ["netflix", "https://www.netflix.com/browse"],
+  ["git", "https://github.com/Dhivakarkd?tab=repositories"],
+]);
+let redisClient;
 
-["youtube","https://www.youtube.com/?gl=IN"],
-["netflix","https://www.netflix.com/browse"],
-["git","https://github.com/Dhivakarkd?tab=repositories"]
+(async () => {
+  redisClient = redis.createClient({ url: process.env.REDIS_URL });
 
-])
+  redisClient.on("error", (error) => console.error(`Error : ${error}`));
 
-// For testing purposes 
-app.get("/:value", (req, res) => { 
+  await redisClient.connect();
+})();
 
-    res.redirect(301, variable.get(req.params.value))
-}); 
+app.use(express.json());
 
-app.listen(PORT, () => { 
-    console.log(`API is listening on port ${PORT}`); 
+app.get("/:value", async (req, res) => {
+    console.log("value is "+req.params.value);
+    const value =  await redisClient.get(req.params.value)
+    console.log(value);
+    res.send(200, value);
+/*     redisClient.get(req.params.value, (err, reply) => {
+    if (err) throw err;
+console.log("Value is "+reply)
+    res.send(200, reply);
+    // res.redirect(301, reply);
+  }); */
+});
+
+app.post("/add/insert",async (req, res) => {
+  console.log(`API is listening on get /add`);
+  console.log(req.body.value);
+  await redisClient.set(req.body.key, req.body.value);
+  res.send(200, req.body);
+});
+
+app.get("/nothing/no", async (req, res) => {
+  console.log("Inside nothing")
+
+  await redisClient.keys('*', function (err, keys) {
+    if (err) return console.log(err);
+     
+    for(var i = 0, len = keys.length; i < len; i++) {
+      console.log(keys[i]);
+    }
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`API is listening on port ${PORT}`);
 });
